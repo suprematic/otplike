a9183736641cae071898a7e6e28073504e60eaf7
Alexey Aristov <aav@acm.org>
2016-05-16 00:25:30 +0200

    initial project.clj and .gitignore

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject suprematic/otplike "0.0.1-SNAPSHOT"
+  :description "Erlang/OTP like processes and behaviours based on core.async"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url  "https://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.match "0.3.0-alpha4"]]
+
+  :plugins [[lein-ancient "0.6.10"]])
