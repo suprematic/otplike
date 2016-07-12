c2332d6e9e11a176b915842b8f5450ed3b5a6723
Semyon Skorokhodov <skorohodovsemen@gmail.com>
2016-07-12 20:22:11 +0300

    update core.async lib

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [org.clojure/core.async "0.2.385"]
                  [org.clojure/core.match "0.3.0-alpha4"]]
 
   :plugins [[lein-ancient "0.6.10"]])
