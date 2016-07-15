0093aa7c552aa91c0cc3507cdb94094e361602c4
Semyon Skorokhodov <skorohodovsemen@gmail.com>
2016-07-15 10:16:32 +0300

    util/stack-trace to get exception stack trace in clojure data structures

--- src/otplike/util.clj
+++ src/otplike/util.clj
@@ -7,3 +7,15 @@
       (check-args ~(rest exprs))
       (throw (IllegalArgumentException.
                (str "require " '~expr " to be true"))))))
+
+(defn stack-trace
+  ([^Throwable e]
+   (let [s-trace (stack-trace e '())]
+     (reduce (fn [acc x] (assoc x :cause acc)) (first s-trace) (rest s-trace))))
+  ([^Throwable e acc]
+   (if e
+     (recur (.getCause e)
+            (conj acc {:message (.getMessage e)
+                       :class (.getName (class e))
+                       :stack-trace (mapv str (.getStackTrace e))}))
+     acc)))
