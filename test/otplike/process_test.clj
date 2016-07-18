f0d8577c10574ece4c93f80a3bd62f94ffa096f7
Semyon Skorokhodov <skorohodovsemen@gmail.com>
2016-07-18 11:55:04 +0300

    move trace tests from process-test to trace-test

--- test/otplike/process_test.clj
+++ test/otplike/process_test.clj
@@ -3,11 +3,10 @@
             [otplike.process :as process :refer [! proc-fn]]
             [otplike.trace :as trace]
             [otplike.test-util :refer :all]
-            [clojure.core.async :as async :refer [<!! <! >! put! go go-loop]]
-            [clojure.core.async.impl.protocols :as ap]
+            [clojure.core.async :as async :refer [<!! <!]]
             [clojure.core.match :refer [match]]))
 
-(trace/set-trace  (fn  [_pid _event]))
+(trace/set-trace (fn [_pid _event]))
 
 ;; ====================================================================
 ;; (self [])
@@ -1555,62 +1554,3 @@
 ;;   process.
 
 
-
-; -----------
-
-#_(deftest spawn-terminate-normal
-  (let [result (trace/trace-collector [:p1])]
-    (process/spawn
-      (fn [inbox p1 p2 & other]
-        (is (process/pid? (process/self)) "self must be instance of Pid")
-        (is (satisfies? ap/ReadPort inbox) "inbox must be a ReadPort")
-        (is (and (= p1 :p1) (= p2 :p2) "formal parameters must match actuals"))
-        (is (= (count other) 0) "no extra parameters")
-        :normal)
-      [:p1 :p2]
-      {:name :p1})
-    (let [trace (result 1000)]
-      (is
-        (match trace
-          [[_ _ [:start _ _ _]]
-           [_ _ [:return :normal]]
-           [_ _ [:terminate :normal]]] true)))))
-
-#_(deftest spawn-terminate-nil
-  (let [result (trace/trace-collector [:p1])]
-    (process/spawn (fn [_inbox]) [] {:name :p1})
-    (let [trace (result 1000)]
-      (is
-        (match trace
-          [[_ _ [:start _ _ _]]
-           [_ _ [:return :nil]]
-           [_ _ [:terminate :nil]]] true)))))
-
-#_(defn- link-to-normal*
-  (let [p1-fn (fn [_inbox]
-                (go
-                  (async/<! (async/timeout 500))
-                  :blah))
-        p2-fn (fn [inbox] (go (<! inbox)))
-        p1 (process/spawn p1-fn [] {:name :p1})
-        p2 (process/spawn p2-fn [] {:name :p2 :link-to p1})] [p1 p2]))
-
-#_(deftest link-to-normal
-  (let [result (trace/trace-collector [:p1 :p2])
-        [p1 p2] (link-to-normal*)
-        trace (result 1000)]
-    (is (trace/terminated? trace p1 :blah))
-    (is (trace/terminated? trace p2 :blah))))
-
-#_(defn- link-to-terminated*
-  (let [p1 (process/spawn (fn [_inbox]) [] {:name :p1})
-        _  (async/<!! (async/timeout 500))
-        p2 (process/spawn (fn [inbox] (go (<! inbox))) [] {:name :p2 :link-to p1})]
-    [p1 p2]))
-
-#_(deftest link-to-terminated
-  (let [result (trace/trace-collector [:p1 :p2])
-        [p1 p2] (link-to-terminated*)
-        trace (result 1000)]
-    (is (trace/terminated? trace p1 :nil))
-    (is (trace/terminated? trace p2 :noproc))))
---
+++ test/otplike/trace_test.clj
@@ -0,0 +1,55 @@
+(ns otplike.trace-test
+  (:require [clojure.test :refer [is deftest]]
+            [otplike.trace :as trace]
+            [otplike.test-util :refer :all]
+            [otplike.process :as process]
+            [clojure.core.match :refer [match]]
+            [clojure.core.async :as async :refer [<!]]))
+
+(deftest spawn-terminate-normal
+  (let [result (trace/trace-collector [:p1])
+        pid (process/spawn (process/proc-fn [_inbox]) [] {:name :p1})
+        trace (result 1000)]
+    (is (match trace
+          [[_ (_ :guard #(= pid %)) [:start _ [] {:name :p1}]]
+           [_ (_ :guard #(= pid %)) [:return :normal]]
+           [_ (_ :guard #(= pid %)) [:terminate :normal]]] true))))
+
+; FIXME
+#_(deftest spawn-terminate-abnormal
+  (let [result (trace/trace-collector [:p1])
+        pfn (process/proc-fn [_inbox] (process/exit (process/self) :abnormal))
+        pid (process/spawn pfn [] {:name :p1})]
+    (let [trace (result 1000)]
+      (is (match trace
+            [[_ (_ :guard #(= pid %)) [:start _ [] {:name :p1}]]
+             [_ (_ :guard #(= pid %)) [:return :abnormal]]
+             [_ (_ :guard #(= pid %)) [:terminate :abnormal]]] true)))))
+
+(deftest link-to-normal
+  (let [result (trace/trace-collector [:p1 :p2])
+        done (async/chan)
+        p1-fn (process/proc-fn [_inbox]
+                (is (await-completion done 100) "test failed")
+                (process/exit (process/self) :blah))
+        p2-fn (process/proc-fn [inbox]
+                (async/close! done)
+                (is (= :inbox-closed (<! (await-message inbox 100)))
+                    "test failed"))
+        p1 (process/spawn p1-fn [] {:name :p1})
+        p2 (process/spawn p2-fn [] {:name :p2 :link-to p1})
+        trace (result 1000)]
+    (is (trace/terminated? trace p1 :blah))
+    (is (trace/terminated? trace p2 :blah))))
+
+(deftest link-to-terminated
+  (let [result (trace/trace-collector [:p1 :p2])
+        p1 (process/spawn (process/proc-fn [_inbox]) [] {:name :p1})
+        _  (async/<!! (async/timeout 50))
+        pfn2 (process/proc-fn [inbox]
+               (is (= :inbox-closed (<! (await-message inbox 100)))
+                   "test failed"))
+        p2 (process/spawn pfn2 [] {:name :p2 :link-to p1})
+        trace (result 1000)]
+    (is (trace/terminated? trace p1 :normal))
+    (is (trace/terminated? trace p2 :noproc))))
