e6ffd2d03fedb4f458a77c27726f922d9b762002
Semyon Skorokhodov <skorohodovsemen@gmail.com>
2016-08-30 10:12:02 +0300

    workaround for lein parallel-test
    lein parallel-test fails, because the plugin loads the old version of
    core.async on start, so otplike can not be compiled. The workaround is
    to tell parallel-test the right version of core.async as a part of
    special :parallel-test profile.
    Unfortunately, parallel-test has a bug preventing custom configuration
    from being used. Thus the patched version of the plugin is required at
    the moment.

--- project.clj
+++ project.clj
@@ -1,13 +1,18 @@
+(def core-async-version "0.2.385")
+
 (defproject otplike/otplike "0.2.0-alpha-SNAPSHOT"
   :description "Erlang/OTP like processes and behaviours on top of core.async"
   :license {:name "Eclipse Public License - v1.0"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.385"]
+                 [org.clojure/core.async ~core-async-version]
                  [org.clojure/core.match "0.3.0-alpha4"]]
 
   :source-paths  ["src" "examples"]
+
+  :profiles {:parallel-test
+             {:dependencies [[org.clojure/core.async ~core-async-version]]}}
 
   :codox {:source-paths ["src"]}
 
