c775704226f0e1561b2af2ab398f8bd1eb989840
Semyon Skorokhodov <skorohodovsemen@gmail.com>
2016-08-30 19:43:44 +0300

    remove redundant locking from process/spawn

--- src/otplike/process.clj
+++ src/otplike/process.clj
@@ -591,53 +591,52 @@
         linked    (ref #{})
         monitors  (ref {})
         flags     (ref (or flags {}))]
-    (locking *processes
-      (let [outbox  (outbox pid inbox)
-            process (new-process
-                      pid inbox control kill monitors exit outbox linked flags)]
-        (sync-register pid process register)
-        (trace pid [:start (str proc-func) args options])
-        ; FIXME bindings from folded binding blocks are stacked, so no values
-        ; bound between bottom and top folded binding blocks are garbage
-        ; collected; see "ring" benchmark example
-        ; FIXME workaround for ASYNC-170. once fixed, binding should move to
-        ; (start-process...)
-        (binding [*self* pid
-                  *inbox* outbox]
-          (go
-            (when link-to
-              (doseq [link-to (set (flatten [link-to]))]
-                (<! (two-phase process link-to link-fn))))
-            (start-process pid proc-func args)
-            (loop []
-              (let [proceed (match (async/alts! [kill control] :priority true)
-                              [val control]
-                              (let [proceed (dispatch-control process val)]
-                                (if (satisfies? ap/ReadPort proceed)
-                                  (<! proceed)
-                                  proceed))
+    (let [outbox  (outbox pid inbox)
+          process (new-process
+                    pid inbox control kill monitors exit outbox linked flags)]
+      (sync-register pid process register)
+      (trace pid [:start (str proc-func) args options])
+      ; FIXME bindings from folded binding blocks are stacked, so no values
+      ; bound between bottom and top folded binding blocks are garbage
+      ; collected; see "ring" benchmark example
+      ; FIXME workaround for ASYNC-170. once fixed, binding should move to
+      ; (start-process...)
+      (binding [*self* pid
+                *inbox* outbox]
+        (go
+          (when link-to
+            (doseq [link-to (set (flatten [link-to]))]
+              (<! (two-phase process link-to link-fn))))
+          (start-process pid proc-func args)
+          (loop []
+            (let [proceed (match (async/alts! [kill control] :priority true)
+                            [val control]
+                            (let [proceed (dispatch-control process val)]
+                              (if (satisfies? ap/ReadPort proceed)
+                                (<! proceed)
+                                proceed))
 
-                              [val kill]
-                              (do
-                                (trace pid [:kill (or val :nil)])
-                                [::break (if (some? val) val :nil)]))]
-                (match proceed
-                  ::continue
-                  (recur)
+                            [val kill]
+                            (do
+                              (trace pid [:kill (or val :nil)])
+                              [::break (if (some? val) val :nil)]))]
+              (match proceed
+                ::continue
+                (recur)
 
-                  [::break reason]
-                  (do
-                    (trace pid [:terminate reason])
-                    (close! outbox)
-                    (dosync
-                      (sync-unregister pid)
-                      (doseq [p @linked]
-                        (when-let [p (@*processes p)]
-                          (alter (:linked p) disj process))))
+                [::break reason]
+                (do
+                  (trace pid [:terminate reason])
+                  (close! outbox)
+                  (dosync
+                    (sync-unregister pid)
                     (doseq [p @linked]
-                      (!control p [:exit pid reason]))
-                    (doseq [[mref [pid object]] @monitors]
-                      (! pid (monitor-message mref object reason)))))))))))
+                      (when-let [p (@*processes p)]
+                        (alter (:linked p) disj process))))
+                  (doseq [p @linked]
+                    (!control p [:exit pid reason]))
+                  (doseq [[mref [pid object]] @monitors]
+                    (! pid (monitor-message mref object reason))))))))))
     pid))
 
 (defn spawn-link
