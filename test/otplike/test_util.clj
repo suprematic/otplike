8a67302c88c471f887d7967c5d5afd36ca0316ba
Simon Skorokhodov <samu3l@Simons-MacBook-Pro.fritz.box>
2017-03-07 13:19:32 +0100

    docstrings for test-util fns

--- test/otplike/test_util.clj
+++ test/otplike/test_util.clj
@@ -3,10 +3,19 @@
             [clojure.core.async :as async]
             [clojure.core.match :refer [match]]))
 
-(defn uuid-keyword []
+(defn uuid-keyword
+  "Makes random keyword with a name being UUID string."
+  []
   (keyword (str (java.util.UUID/randomUUID))))
 
-(defn await-message [timeout-ms]
+(defn await-message
+  "Tries to receive message. Returns:
+    - :timeout if no message appears during timeout-ms
+    - [:exit [pid reason]] if :EXIT message received
+    - [:down [ref object reason]] if :DOWN message received
+    - [:message message] on any other message
+    - nil if inbox becomes closed before message appears"
+  [timeout-ms]
   (async/go
     (try
       (process/receive!
@@ -18,7 +27,10 @@
       (catch Exception _e
         nil))))
 
-(defn await-completion [chan timeout-ms]
+
+(defn await-completion
+  "Returns :ok if chan is closed during timeout-ms, otherwise throws."
+  [chan timeout-ms]
   (let [timeout (async/timeout timeout-ms)]
     (match (async/alts!! [chan timeout])
       [nil chan] :ok
