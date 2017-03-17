eaeedceba0c14cf00e7c1ead9a3cc277fdbd28a1
Simon Skorokhodov <samu3l@Simons-MBP.fritz.box>
2017-03-17 11:59:51 +0100

    add proc-util/execute-proc to execute form in process context

--- src/otplike/proc_util.clj
+++ src/otplike/proc_util.clj
@@ -2,6 +2,22 @@
   (:require [clojure.core.async :as async]
             [otplike.process :as process]))
 
+(defmacro execute-proc
+  "Executes body in newly created process context."
+  [& body]
+  `(let [done# (async/chan)]
+     (process/spawn
+       (process/proc-fn
+         []
+         (try
+           (let [res# (do ~@body)]
+             (when (some? res#) (async/>! done# res#)))
+           (finally
+             (async/close! done#))))
+       []
+       {})
+     (async/<!! done#)))
+
 (defmacro defn-proc
   "Defines function with name fname, arguments args, which body is
   executed in newly created process context."
