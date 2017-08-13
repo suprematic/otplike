97a2d21fcbaa58fe0ea770076c28008fec657257
Simon Skorokhodov <skorohodovsemen@gmail.com>
2017-08-13 10:37:39 +0200

    add explanatory printing to example 4 (issue #19)

--- examples/otplike/example/e4_receive_timeout.clj
+++ examples/otplike/example/e4_receive_timeout.clj
@@ -1,20 +1,28 @@
 (ns otplike.example.e4-receive-timeout
   (:require [otplike.process :as process :refer [!]]
-            [clojure.core.async :as async]
-            [otplike.proc-util :as proc-util]))
+            [clojure.core.async :as async]))
 
 (process/proc-defn await-message [timeout]
-  (process/receive!
-    msg (printf "receive %s%n" msg)
-    (after timeout
-      (printf "timeout %s%n" timeout))))
+  (let [self (process/self)]
+    (process/receive!
+      msg (printf "[%s] receive %s%n" self msg)
+      (after timeout
+             (printf "[%s] timeout %s%n" self timeout)))))
 
 (defn run []
-  (process/spawn await-message [1000])
-  (process/spawn await-message [(async/timeout 1000)])
-  (let [pid (process/spawn await-message [:infinity])]
-    (Thread/sleep 2000)
-    (! pid :stop)
-    (Thread/sleep 100)))
+  (let [pid0 (process/spawn await-message [1000])]
+    (printf "[main] started %s%n" pid0)
+    (let [pid1 (process/spawn await-message [(async/timeout 1000)])]
+      (printf"[main] started %s%n" pid1)
+      (let [pid2 (process/spawn await-message [:infinity])]
+        (printf "[main] started %s%n" pid2)
+        (printf"[main] waiting for 2 seconds%n")
+        (Thread/sleep 2000)
+        (printf "[main] sending :stop to %s%n" pid2)
+        (! pid2 :stop)
+        (printf "[main] waiting for 100ms%n")
+        (Thread/sleep 100)
+        (printf "[main] done%n")
+        (flush)))))
 
 #_(run)
