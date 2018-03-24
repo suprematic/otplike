ddecdfa1cffeb68222e3d44eb84a2d5414b9d5aa
Simon Skorokhodov <skorohodovsemen@gmail.com>
2018-03-24 23:04:24 +0100

    actualize project version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (def core-async-version "0.4.474")
 
-(defproject otplike/otplike "0.3.0-alpha-SNAPSHOT"
+(defproject otplike/otplike "0.4.0-alpha-SNAPSHOT"
   :description "Erlang/OTP like processes and behaviours on top of core.async"
   :license {:name "Eclipse Public License - v1.0"
             :url  "https://www.eclipse.org/legal/epl-v10.html"}
