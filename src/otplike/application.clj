4f2cd9473fc3c1e0e3bbf269533b875dc9e12d22
Alexey Aristov <aav@acm.org>
2019-10-12 20:01:15 +0200

    - initial spec for application.clj, clieanup

--- src/otplike/application.clj
+++ src/otplike/application.clj
@@ -1,5 +1,6 @@
 (ns otplike.application
   (:require
+   [clojure.spec.alpha :as spec]
    [clojure.java.io :as io]
    [clojure.set :as set]
    [clojure.core.match :refer [match]]
@@ -7,71 +8,105 @@
    [otplike.util :as u]
    [otplike.process :as process]
    [otplike.proc-util :as proc-util]
-   [otplike.gen-server :as gs]))
+   [otplike.gen-server :as gs]
+   [otplike.spec-util :as spec-util]))
+
+(when (and (= 1 (:major *clojure-version*))
+        (< (:minor *clojure-version*) 9))
+  (require '[clojure.future :refer :all]))
 
 (defn debug [pattern & args]
   (let [str (apply format pattern args)]
     (println "DEBUG:" str)))
 
-(defn start-link []
-  (gs/start-link-ns ::application-controller [] {}))
 
-(defn start
+(spec/def ::ok #{:ok})
+(spec/def ::error #{:error})
+
+(spec/def ::name symbol?)
+(spec/def ::resource string?)
+(spec/def ::applications (spec/coll-of symbol?))
+(spec/def ::namespaces (spec/coll-of symbol?))
+
+(spec/def ::app-descr
+  (spec/keys
+    :req [::name ::resource ::applications ::namespaces]))
+
+(spec/def ::path (spec/coll-of symbol?))
+
+(spec/fdef load-application
+  :args (spec/or
+         :v1 (spec/cat :name symbol?)
+         :v2 (spec/cat :name symbol? :path ::path))
+  :ret (spec/or
+         :ok (spec/tuple ::ok ::app-descr)
+         :error (spec/tuple ::error (spec/tuple keyword? ::path))))
+(defn- load-application
   ([name]
-   (start name true))
-  ([name permanent?]
-   (gs/call ::application-controller [::start name permanent?])))
+   (load-application name []))
+  ([name path]
+   (let [resource (format "%s.app.edn" name)]
+     (if-let [stream (io/resource resource)]
+       (try
+         (let [{:keys [namespaces applications]} (-> stream slurp read-string)]
+           [:ok
+            {::name name
+             ::resource resource
+             ::applications (map symbol applications)
+             ::namespaces (map symbol namespaces)}])
+         (catch Throwable t
+           [:error [:bad-file path]]))
+       [:error [:no-file path]]))))
+(spec-util/instrument 'load-application)
 
-(defn stop [name]
-  (gs/call ::application-controller [::stop name]))
+#_(load-application 'kernel)
 
-(def normalize symbol)
-
-(defn- load-application [name path]
-  (let [name (normalize name)
-        resource (format "%s.app.edn" name)]
-    (if-let [stream (io/resource resource)]
-      (try
-        (let [resource
-              (-> stream slurp read-string
-                (assoc :name name :resource resource)
-                (update :applications #(->> % (map normalize) (apply hash-set)))
-                (update :namespace
-                  #(if (coll? %)
-                     (map normalize %)
-                     [(normalize %)])))]
-          [:ok resource])
-        (catch Throwable t
-          (.printStackTrace t)
-          [:error {:reason :bad-app-file :name name :resource resource :path path}]))
-      [:error {:reason :no-file :name name :resource resource :path path}])))
-
-(defn- load-apps-resources
+(spec/fdef load-applications
+  :args (spec/or
+          :v1 (spec/cat :name symbol?)
+          :v2 (spec/cat
+                :name symbol
+                :apps (spec/coll-of ::app-descr)
+                :loaded any?
+                :path ::path))
+  :ret (spec/or
+         :ok (spec/tuple ::ok (spec/coll-of ::app-descr))
+         :error (spec/tuple ::error (spec/tuple keyword? ::path))))
+(defn- load-applications
   ([name]
-   (load-apps-resources name [{} {}] [name]))
-  ([name [loaded errors] path]
-   (if-not (loaded name)
+   (try
+     [:ok (load-applications name '()  (atom #{}) [name])]
+     (catch clojure.lang.ExceptionInfo ex
+       [:error (:reason (ex-data ex))])))
+  ([name applications loaded? path]
+   (if-not (@loaded? name)
      (match (load-application name path)
        [:ok application]
-       (reduce
-         (fn [[loaded errors] name]
-           (load-apps-resources name [loaded errors] (conj path name)))
-         [(assoc loaded name application) errors]
-         (:applications application))
-       [:error error]
-       [loaded (assoc errors name error)])
-     [loaded errors])))
+       (do 
+         (swap! loaded? conj name)
+         (let [deps
+               (mapcat
+                 (fn [name]
+                   (load-applications name applications loaded? (conj path name)))
+                 (:applications application))]
+           (concat deps [application] applications)))
+       [:error reason]
+       (throw (ex-info "" {:reason reason})))
+     applications)))
+(spec-util/instrument 'load-applications)
 
-(process/proc-defn- application-master-p [{:keys [name namespace start-fn stop-fn] :as application} controller-pid]
+#_(load-applications 'kernel1)
+
+(process/proc-defn- application-master-p [{:keys [name namespaces start-fn stop-fn] :as application} controller-pid]
   (process/flag :trap-exit true)
   (when-let
       [[sup-pid state]
        (try
-         (doseq [ns namespace]
+         (doseq [ns namespaces]
            (require ns :reload))
 
          (if-let [start-fn (some-> start-fn resolve var-get)] 
-           (match (process/await?! (apply start-fn []))
+           (match (process/await?! (apply start-fn [{:name name}]))
              [:ok (sup-pid :guard process/pid?) state]
              [sup-pid state]
 
@@ -79,12 +114,12 @@
              [sup-pid nil]
 
              bad-return
-             (process/exit [:error [:bad-return bad-return]]))
-           (process/exit [:error :no-start-fn]))
+             (process/exit [:bad-return bad-return]))
+           (process/exit :no-start-fn))
          (catch clojure.lang.ExceptionInfo ex
            (throw ex))
          (catch Throwable ex
-           (process/exit [:error [:exception (u/stack-trace ex)]])))]
+           (process/exit [:exception (u/stack-trace ex)])))]
 
     (process/! controller-pid [:started (process/self)])
 
@@ -113,25 +148,46 @@
   (process/flag :trap-exit true)
   [:ok {::started '()}])
 
-(defn filter-rev-comp [preds coll]
-  (let [pred (apply comp (reverse preds))]
-    (filter pred coll)))
-
+(defn- start-many [applications permanent?]
+  (process/async
+    (loop [applications applications new-started '()]
+      (if (empty? applications)
+        [:ok new-started]
+        (let [[{:keys [name] :as application} & remaining] applications]
+          (debug "starting application: %s" name)
+          
+          (match (let [app-pid (process/spawn-link application-master-p [application (process/self)])]
+                   (process/selective-receive!
+                     [:started app-pid]
+                     (do
+                       (debug "application master started for %s pid=%s" name app-pid)
+                       [:ok {:application application :pid app-pid :permanent? permanent?}])
+                     
+                     [:EXIT app-pid reason]
+                     (do
+                       (debug "application master exit for %s reason=%s, pid=%s" name reason app-pid)
+                       [:error reason])))
+            
+            [:ok application']
+            (recur remaining (conj new-started application'))
+            
+            [:error reason]
+            [:error new-started reason]))))))
+ 
 (defn handle-call [message _reply-to {:keys [started] :as state}]
   (process/async
     (match message
       [::which]
       [:reply
-       (into '()
+       (apply list
          (map
            (fn [{:keys [application]}]
              [(get application :name) (get application :description "") (get application :version "0.0.0")]) started)) state]
 
       [::start name permanent?]
-      (let [name (normalize name)
-            started-names (->> started (map (comp :name :application)) (apply hash-set))]
+      (let [started-names (->> started (map (comp :name :application)) (apply hash-set))]
         (if-not (contains? started-names name)
-          (match (load-application name [])
+          (match (load-application name)
             [:ok application]
             (let [not-started
                   (set/difference
@@ -152,26 +208,49 @@
                     [:EXIT app-pid reason]
                     (do
                       (debug "application master exit for %s reason=%s, pid=%s" name reason app-pid)
-                      [:reply [:error reason] state])))))
+                      [:reply [:error name reason] state])))))
 
             [:error error]
             [:reply [:error error] state])
           [:reply [:error [:already-started name]] state]))
 
+      [::start-all name permanent?]
+      (match (load-applications name)
+        [:ok applications]
+        (let [applications
+              (let [started? (->> started (map (comp :name :application)) (apply hash-set))]
+                (filter
+                  (fn [{:keys [name]}]
+                    (not (started? name))) applications))]
+          
+          (debug "start many: %s" (apply list applications))
+
+          (match (process/await! (start-many applications permanent?))
+            [:ok new-started]
+            (do
+              (debug "new-started: %s" new-started)
+              [:reply :ok (assoc state :started (concat new-started started))])
+
+            [:error new-started reason]
+            (do
+              (debug "new-started: %s" new-started)
+              [:reply [:error reason] (assoc state :started (concat new-started started))])))
+
+        [:error errors]
+        [:reply [:error errors] state])
+
       [::stop name]
-      (let [name (normalize name)]
-        (if-let [app-pid (->> started (filter (comp #(= % name) :name :application)) first :pid)]
-          (do
-            (debug "requesting application master to stop pid=%s, reason=%s" app-pid :normal)
-            (process/exit app-pid :normal)
-            
-            (process/selective-receive!
-              [:EXIT app-pid reason]
-              (do
-                (debug "application master exit pid=%s, reason=%s" app-pid reason)
-                [:reply :ok
-                 (assoc state :started (filter (comp #(not= % name) :name :application) started))])))
-          [:reply [:error [:not-started name]] state])))))
+      (if-let [app-pid (->> started (filter (comp #(= % name) :name :application)) first :pid)]
+        (do
+          (debug "requesting application master to stop pid=%s, reason=%s" app-pid :normal)
+          (process/exit app-pid :normal)
+          (process/selective-receive!
+            [:EXIT app-pid reason]
+            (do
+              (debug "application master exit pid=%s, reason=%s" app-pid reason)
+              [:reply :ok
+               (assoc state :started (filter (comp #(not= % name) :name :application) started))])))
+        [:reply [:error [:not-started name]] state]))))
 
 (defn handle-info [message {:keys [started] :as state}]
   (process/async
@@ -196,15 +275,52 @@
   (debug "terminate: %s" reason)
   [:noreply state])
 
+(defn start-link []
+  (gs/start-link-ns ::application-controller [] {}))
+
+(defn start
+  ([name]
+   (start name true))
+  ([name permanent?]
+   (gs/call ::application-controller [::start name permanent?])))
+
+(defn start-all
+  ([name]
+   (start-all name true))
+  ([name permanent?]
+   (gs/call ::application-controller [::start-all name permanent?])))
+
+(defn stop [name]
+  (gs/call ::application-controller [::stop name]))
+
+(defn which []
+  (gs/call ::application-controller [::which]))
+
+#_(load-applications 'dep1)
 
 #_(proc-util/execute-proc!!
     (gs/call! ::application-controller [::start 'dep1 true]))
 
 #_(proc-util/execute-proc!!
+  (gs/call! ::application-controller [::start 'dep2 true]))
+
+#_(proc-util/execute-proc!!
+    (gs/call! ::application-controller [::start-all 'dep1 true]))
+
+#_(proc-util/execute-proc!!
+    (gs/call! ::application-controller [::start-all 'dep2 true]))
+
+#_(proc-util/execute-proc!!
     (gs/call! ::application-controller [::start 'kernel true]))
 
 #_(proc-util/execute-proc!!
-    (gs/call! ::application-controller [::stop 'kernel true]))
+    (gs/call! ::application-controller [::stop 'dep1]))
+
+#_(proc-util/execute-proc!!
+    (gs/call! ::application-controller [::stop 'dep2]))
+
+#_(proc-util/execute-proc!!
+    (gs/call! ::application-controller [::stop 'dep3]))
 
 #_(proc-util/execute-proc!!
     (gs/call! ::application-controller [::which]))
