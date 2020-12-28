06a5922139a4f85ce21e42aabe68b85e8e1bd905
Alexey Aristov <aav@suprematic.de>
2020-12-28 22:42:18 +0100

    kernel: set logging configuration on application start

--- src/otplike/kernel.clj
+++ src/otplike/kernel.clj
@@ -1,11 +1,13 @@
 (ns otplike.kernel
   (:require
-   [otplike.supervisor :as supervisor]))
+   [otplike.supervisor :as supervisor]
+   [otplike.logger :as logger]))
 
 (defn- sup-fn []
   [:ok
    [{:strategy :one-for-all}
     []]])
 
-(defn start [_env]
+(defn start [{:keys [logger]}]
+  (logger/set-config! logger)
   (supervisor/start-link sup-fn []))
